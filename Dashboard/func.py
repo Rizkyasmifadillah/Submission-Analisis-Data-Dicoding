7883d212b957ed627cdc6d96d9d5769c2836c028
Rizkyasmifadillah <m384b4kx3926@bangkit.academy>
2024-10-02 11:13:08 +0700

    Add files via upload

---
+++ Dashboard/func.py
@@ -0,0 +1,65 @@
+from typing import Self
+import pandas as pd
+
+class DataAnalyzer:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def create_daily_orders_df(self) -> pd.DataFrame:
+        daily_orders_df = self.df.resample(rule='D', on='order_approved_at').agg({
+            "order_id": "nunique",
+            "payment_value": "sum"
+        })
+        daily_orders_df = daily_orders_df.reset_index()
+        daily_orders_df.rename(columns={
+            "order_id": "order_count",
+            "payment_value": "revenue"
+        }, inplace=True)
+        
+        return daily_orders_df
+    
+    def create_sum_spend_df(self) -> pd.DataFrame:
+        sum_spend_df = self.df.resample(rule='D', on='order_approved_at').agg({
+            "payment_value": "sum"
+        })
+        sum_spend_df = sum_spend_df.reset_index()
+        sum_spend_df.rename(columns={
+            "payment_value": "total_spend"
+        }, inplace=True)
+
+        return sum_spend_df
+
+    def create_sum_order_items_df(self) -> pd.DataFrame:
+        sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
+        sum_order_items_df.rename(columns={
+            "product_id": "product_count"
+        }, inplace=True)
+        sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
+
+        return sum_order_items_df
+
+    def review_score_df(self) -> tuple:
+        review_scores = self.df['review_score'].value_counts().sort_values(ascending=False)
+        most_common_score = review_scores.idxmax()
+
+        return review_scores, most_common_score
+
+    def create_bystate_df(self) -> tuple:
+        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index()
+        bystate_df.rename(columns={
+            "customer_id": "customer_count"
+        }, inplace=True)
+        most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
+        bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
+
+        return bystate_df, most_common_state
+
+    def create_order_status(self) -> tuple:
+        order_status_df = self.df["order_status"].value_counts().sort_values(ascending=False)
+        most_common_status = order_status_df.idxmax()
+
+        return order_status_df, most_common_status
+
+class BrazilMapPlotter:
+    def __init__(self, data):
+        self.data = data
